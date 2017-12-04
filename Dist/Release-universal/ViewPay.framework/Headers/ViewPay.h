f5906864e311b80352fd10f8ed1d0e26f648398e
Thibaut LE LEVIER <thibaut@lelevier.fr>
2017-12-04 18:49:14 +0100

    update on project configuration and wip on distribution scripts

---
+++ Dist/Release-universal/ViewPay.framework/Headers/ViewPay.h
@@ -0,0 +1,24 @@
+//
+//  ViewPay.h
+//  ViewPay
+//
+//  Created by Thibaut LE LEVIER on 29/11/2017.
+//  Copyright © 2017 ViewPay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewPay.
+FOUNDATION_EXPORT double ViewPayVersionNumber;
+
+//! Project version string for ViewPay.
+FOUNDATION_EXPORT const unsigned char ViewPayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewPay/PublicHeader.h>
+
+@interface ViewPay : NSObject
+
++ (void)sayHello;
+
+@end
---
+++ Dist/Release-universal/ViewPay.framework/PrivateHeaders/ViewPay_private.h
@@ -0,0 +1,9 @@
+//
+//  ViewPay_private.h
+//  ViewPay
+//
+//  Created by Thibaut LE LEVIER on 29/11/2017.
+//  Copyright © 2017 ViewPay. All rights reserved.
+//
+
+#import "ViewPay.h"
